b18c2ffdfac03d90a285967f9cd9f8c1c25b0b68
Reynaldi Priatna <rey.twenty.one@gmail.com>
2023-10-10 13:40:18 +0700

    Update Apps

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.get("/app2", (req, res) => {
   res.send("Hello this App 2!");
 });
 
-app.get("/reynaldi", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.get("/users", (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc";
   connection.query(sql, (error, fields) => {
